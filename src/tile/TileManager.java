f5c2b3c81e9fd474521ce60a6f8a10f5d392a58e
Nuno Fonseca <nunofonsecaflorencio@gmail.com>
2021-12-04 12:27:55 +0200

    Sistema de mapa, classe para cada bloco, edicao de mapa atravez de aquivo de texto

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -9,14 +9,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+
 
 public class TileManager {
 
     GamePanel gp;
     Tile[] tiles;
-    int mapTile[][];
+    int[][] mapTile;
 
     public  TileManager(GamePanel gp){
         this.gp = gp;
