aa08914ed30ef0c1d26573196f6b96ce36ac96ac
Nuno Fonseca <nunofonsecaflorencio@gmail.com>
2021-12-04 13:12:31 +0200

    Imagem do Player, animacao da sprite.

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,6 +1,14 @@
 package entity;
 
+import java.awt.image.BufferedImage;
+
 public class Entity {
     public int x, y;
     public int speed;
+    BufferedImage p1, p2;
+    boolean moving = false;
+
+    int spriteNum = 1;
+    int spriteCounter = 0;
+    int spriteInterval;
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -3,8 +3,11 @@ package entity;
 import game.GamePanel;
 import game.KeyHandler;
 
-import java.awt.Color;
+import javax.imageio.ImageIO;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Player extends Entity{
     GamePanel gp;
@@ -13,32 +16,71 @@ public class Player extends Entity{
     public Player(GamePanel gp, KeyHandler keys){
         this.gp = gp;
         this.keys = keys;
+        spriteInterval =20;
         setDefaultValues();
+        getPlayerImage();
     }
+
+    private void getPlayerImage() {
+        try{
+            p1 = ImageIO.read(new File("res/player/player1.png"));
+            p2 = ImageIO.read(new File("res/player/player2.png"));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
     public void setDefaultValues(){
         x = 100;
         y = 100;
         speed = 4;
     }
     public void update(){
+        moving = false;
         if(keys.wPressed){
             y -= speed;
+            moving = true;
         }
         else if(keys.sPressed) {
             y += speed;
+            moving = true;
         }
         else if(keys.aPressed) {
             x -= speed;
+            moving = true;
         }
         else if(keys.dPressed) {
             x += speed;
+            moving = true;
         }
         else if(keys.enterPressed) {
             setDefaultValues();
         }
+
+        if (moving){
+            spriteCounter++;
+
+            if(spriteCounter > spriteInterval){
+                if (spriteNum == 2)
+                    spriteNum = 1;
+                else if (spriteNum == 1)
+                    spriteNum = 2;
+
+                spriteCounter = 0;
+            }
+        }else {
+            spriteNum = 1;
+        }
+
     }
     public void draw(Graphics2D g2){
-        g2.setColor(Color.white);
-        g2.fillRect(x, y, gp.TILE_SIZE, gp.TILE_SIZE);
+        BufferedImage image = null;
+
+        if (spriteNum == 1)
+            image = p1;
+        else if (spriteNum == 2)
+            image = p2;
+
+       g2.drawImage(image, x, y, gp.TILE_SIZE, gp.TILE_SIZE, null);
     }
 }
